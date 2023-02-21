14907bbae52bf84c5538215d1e8533aca577ea63
HilbertSpaces <mvii@Jonathans-MacBook-Pro-2.local>
2023-02-21 16:53:38 -0500

    first commit

---
+++ basicServer.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, after_this_request,request
+import random, string
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	
+	@after_this_request
+	def add_header(response):
+		response.headers.add('Access-Control-Allow-Origin', '*')
+		return response
+	
+	couponCode = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
+	args = request.args
+
+	jsonResp = {'couponCode': couponCode}
+	return jsonify(jsonResp)
